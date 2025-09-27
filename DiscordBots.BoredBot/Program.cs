914e229ee53a71b4ad728b8895eee9151d324773
Jens Bech-Sørensen <jens@bechsor.no>
2025-09-27 15:58:20 +0200

    factory

--- DiscordBots.BoredBot/Program.cs
+++ DiscordBots.BoredBot/Program.cs
@@ -1,5 +1,20 @@
 using DiscordBots.BoredBot;
 using DiscordBots.Core;
+using Microsoft.Extensions.Hosting;
 
-var environmentVariables = DiscordBot.EnsureEnvironmentVariables();
-_ = await BoredBot.GetInstanceAsync(environmentVariables, BoredBotCommands.Commands);
+namespace DiscordBots.BoredBot;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var host = DiscordBotHostingExtensions.CreateDiscordBotHost<BoredBot>(
+            args,
+            BoredBot.GetInstanceAsync,
+            BoredBotCommands.Commands,
+            "Bored Bot"
+        );
+
+        await host.RunAsync();
+    }
+}
---
+++ DiscordBots.Core/DiscordBotHostingExtensions.cs
@@ -0,0 +1,39 @@
+using Discord;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBots.Core;
+
+public static class DiscordBotHostingExtensions
+{
+    public static IHostApplicationBuilder AddDiscordBot<TBot>(
+        this IHostApplicationBuilder builder,
+        Func<BotEnvironmentVariables, SlashCommandBuilder[], Task<TBot>> botFactory,
+        SlashCommandBuilder[] commands,
+        string botName
+    )
+        where TBot : DiscordBot
+    {
+        builder.Services.AddSingleton(botFactory);
+        builder.Services.AddSingleton(commands);
+        builder.Services.AddSingleton(botName);
+        builder.Services.AddHostedService<DiscordBotService<TBot>>();
+
+        return builder;
+    }
+
+    public static IHost CreateDiscordBotHost<TBot>(
+        string[] args,
+        Func<BotEnvironmentVariables, SlashCommandBuilder[], Task<TBot>> botFactory,
+        SlashCommandBuilder[] commands,
+        string botName
+    )
+        where TBot : DiscordBot
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Logging.AddConsole();
+        builder.AddDiscordBot(botFactory, commands, botName);
+        return builder.Build();
+    }
+}
